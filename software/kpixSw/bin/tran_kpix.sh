e14b1d07f3a9cb1dbd3650162e8ec5254aae1a1e
rherbst <rherbst@slac.stanford.edu>
2011-05-04 23:30:54 +0000

    Fixed.
    git-svn-id: file:///afs/slac/g/reseng/svn/repos/kpix/trunk@850 0848cbd7-7569-0410-ab05-8cc0a0c67708

--- software/kpixSw/bin/tran_kpix.sh
+++ software/kpixSw/bin/tran_kpix.sh
@@ -8,6 +8,7 @@ fi
 
 lftp -c "set ftp:list-options -a;
 open ftp://anonymous:rherbst@slac.stanford.edu@ftp.slac.stanford.edu; 
+mkdir incoming/kpix;
 cd incoming/kpix;
 mirror --reverse \
        --delete \
