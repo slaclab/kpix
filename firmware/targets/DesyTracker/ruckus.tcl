a8be2ab491fb421e8caa73ef1761779fe557170e
Benjamin Reese <bareese@slac.stanford.edu>
2018-05-08 13:51:21 -0700

    Create DesyTracker target

---
+++ firmware/targets/DesyTracker/ruckus.tcl
@@ -0,0 +1,16 @@
+############################
+# DO NOT EDIT THE CODE BELOW
+############################
+
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load submodules' code and constraints
+loadRuckusTcl $::env(TOP_DIR)/submodules/surf
+loadRuckusTcl $::env(TOP_DIR)/common/KpixDaq
+
+# Load target's source code and constraints
+loadSource      -dir  "$::DIR_PATH/rtl/"
+loadSource      -sim_only -dir "$::DIR_PATH/sim/"
+loadConstraints -dir  "$::DIR_PATH/rtl/"
+
