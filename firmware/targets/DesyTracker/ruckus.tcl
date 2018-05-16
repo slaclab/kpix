0aad17ee6e2f660e736a85c8b6e76f0a612e508a
Benjamin Reese <bareese@slac.stanford.edu>
2018-05-16 13:12:41 -0700

    Add/Update ruckus.tcl files

--- firmware/targets/DesyTracker/ruckus.tcl
+++ firmware/targets/DesyTracker/ruckus.tcl
@@ -8,9 +8,10 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load submodules' code and constraints
 loadRuckusTcl $::env(TOP_DIR)/submodules/surf
 loadRuckusTcl $::env(TOP_DIR)/common/KpixDaq
+loadRuckusTcl $::env(TOP_DIR)/common/KpixCore
 
 # Load target's source code and constraints
 loadSource      -dir  "$::DIR_PATH/rtl/"
-loadSource      -sim_only -dir "$::DIR_PATH/sim/"
+#loadSource      -sim_only -dir "$::DIR_PATH/sim/"
 loadConstraints -dir  "$::DIR_PATH/rtl/"
 
