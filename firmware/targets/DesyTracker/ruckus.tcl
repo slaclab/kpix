243856aa6a3547c60fce154f5c569e809402c666
Benjamin Reese <bareese@slac.stanford.edu>
2018-05-21 16:02:39 -0700

    Add simulation sources

--- firmware/targets/DesyTracker/ruckus.tcl
+++ firmware/targets/DesyTracker/ruckus.tcl
@@ -12,6 +12,6 @@ loadRuckusTcl $::env(TOP_DIR)/common/KpixCore
 
 # Load target's source code and constraints
 loadSource      -dir  "$::DIR_PATH/rtl/"
-#loadSource      -sim_only -dir "$::DIR_PATH/sim/"
+loadSource      -sim_only -dir "$::DIR_PATH/sim/"
 loadConstraints -dir  "$::DIR_PATH/rtl/"
 
