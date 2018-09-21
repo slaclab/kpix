29f0633ec6a94ef539dbff67a01f55973aafda34
Benjamin Reese <bareese@slac.stanford.edu>
2018-09-21 16:34:50 -0700

    Add gui script

---
+++ software/scripts/gui.py
@@ -0,0 +1,52 @@
+import rogue
+import KpixDaq
+import pyrogue.gui
+import PyQt4.QtGui
+import sys
+import argparse
+
+rogue.Logging.setFilter('pyrogue.SrpV3', rogue.Logging.Debug)
+# Set the argument parser
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--ip", 
+    type     = str,
+    required = False,
+    default = '192.168.2.10',
+    help     = "IP address",
+)  
+
+parser.add_argument(
+    "--hwEmu", 
+    required = False,
+    action = 'store_true',
+    help     = "hardware emulation (false=normal operation, true=emulation)",
+)
+
+parser.add_argument(
+    "--sim", 
+    required = False,
+    action = 'store_true',
+    help     = "hardware emulation (false=normal operation, true=emulation)",
+)  
+
+parser.add_argument(
+    "--pollEn", 
+    required = False,
+    action = 'store_true',
+    help     = "enable auto-polling",
+)
+
+args = parser.parse_args()
+print(args)
+
+with KpixDaq.DesyTrackerRoot(**vars(args)) as root:
+    # Create GUI
+    appTop = pyrogue.gui.application(sys.argv)
+    guiTop = pyrogue.gui.GuiTop(group='guiGroup')
+    guiTop.addTree(root)
+    guiTop.resize(1000,1000)
+
+    # Run gui
+    appTop.exec_()
