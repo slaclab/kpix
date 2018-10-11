71d48d3dcca27c94c4e50cd622d4999e2a7fa01f
Benjamin Reese <bareese@slac.stanford.edu>
2018-10-11 16:20:56 -0700

    Script for running data analysis on a file

---
+++ software/scripts/ReadFile.py
@@ -0,0 +1,26 @@
+import sys
+import os.path
+import time
+import pyrogue
+import rogue.utilities
+import rogue.utilities.fileio
+
+import KpixDaq
+
+rogue.Logging.setFilter('LegacyStreamReader', rogue.Logging.Debug)
+
+reader = rogue.utilities.fileio.LegacyStreamReader()
+parser = KpixDaq.KpixCalibration()
+
+pyrogue.streamConnect(reader, parser)
+
+def main(args):
+    reader.open(args[1])
+    print('opened', args[1])
+    reader.closeWait()
+    print('closed', args[1])
+    #parser.process()
+    #parser.noise()
+
+if __name__ == "__main__":
+    main(sys.argv)
