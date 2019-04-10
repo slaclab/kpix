4c7c564d47bbe0c9ca6de3ae9d36f87a8226a5cb
Benjamin Reese <bareese@slac.stanford.edu>
2019-04-10 12:18:38 -0700

    Use LinkVariables to hide separate Src and En registers

--- software/python/KpixDaq/_AcquisitionControl.py
+++ software/python/KpixDaq/_AcquisitionControl.py
@@ -7,6 +7,13 @@ class AcquisitionControl(pr.Device):
         if extTrigEnum is None:
             extTrigEnum = {x:str(x) for x in range(8)}
 
+#         for k,v in extTrigEnum.items():
+#             extTrigEnum.pop(k)
+#             n = k | 0b1000
+#             extTrigEnum[n] = k
+
+#         extTrigEnum
+
         self.add(pr.RemoteVariable(
             name = "RunTime",
             mode = 'RO',
@@ -14,9 +21,29 @@ class AcquisitionControl(pr.Device):
             bitSize = 64,
             base = pr.UInt))
 
+        extTrigList = ['Disabled'] + list(extTrigEnum.values())
+        
+        def extSet(dev, var, value, write):
+            if value == 'Disabled':
+                var.dependencies[0].set(0, write=write)
+                var.dependencies[1].set(False, write=write)
+                
+            else:
+                print(f'Setting {var.dependencies[0].path}.setDisp({value})')
+                var.dependencies[0].setDisp(value)
+                var.dependencies[1].set(True)
+
+        def extGet(var):
+            if var.dependencies[1] is False:
+                return 'Disabled'
+            else:
+                return var.dependencies[0].valueDisp()
+
+
         self.add(pr.RemoteVariable(
-            name = 'ExtTrigSrc',
+            name = 'ExtTrigSrcRaw',
             mode = 'RW',
+            hidden = True,
             offset= 0x00,
             bitOffset=0,
             bitSize=3,
@@ -25,14 +52,24 @@ class AcquisitionControl(pr.Device):
         self.add(pr.RemoteVariable(
             name = 'ExtTrigEn',
             mode = 'RW',
+            hidden = True,
             offset= 0x10,
             bitOffset=0,
             bitSize=1,
             base=pr.Bool))
 
-        self.add(pr.RemoteVariable(
-            name = 'ExtTimestampSrc',
+        self.add(pr.LinkVariable(
+            name = 'ExtTrigSrc',
             mode = 'RW',
+            dependencies = [self.ExtTrigSrcRaw, self.ExtTrigEn],
+            disp = extTrigList,
+            linkedGet = extGet,
+            linkedSet = extSet))
+
+        self.add(pr.RemoteVariable(
+            name = 'ExtTimestampSrcRaw',
+            mode = 'RW',
+            hidden = True,
             offset= 0x04,
             bitOffset=0,
             bitSize=3,
@@ -41,14 +78,25 @@ class AcquisitionControl(pr.Device):
         self.add(pr.RemoteVariable(
             name = 'ExtTimestampEn',
             mode = 'RW',
+            hidden = True,
             offset= 0x10,
             bitOffset=1,
             bitSize=1,
             base=pr.Bool))
 
-        self.add(pr.RemoteVariable(
-            name = 'ExtAcquisitionSrc',
+        self.add(pr.LinkVariable(
+            name = 'ExtTimestampSrc',
             mode = 'RW',
+            dependencies = [self.ExtTimestampSrcRaw, self.ExtTimestampEn],
+            disp = extTrigList,
+            linkedGet = extGet,
+            linkedSet = extSet))
+        
+
+        self.add(pr.RemoteVariable(
+            name = 'ExtAcquisitionSrcRaw',
+            mode = 'RW',
+            hidden = True,
             offset= 0x08,
             bitOffset=0,
             bitSize=3,
@@ -57,14 +105,25 @@ class AcquisitionControl(pr.Device):
         self.add(pr.RemoteVariable(
             name = 'ExtAcquisitionEn',
             mode = 'RW',
+            hidden = True,
             offset= 0x10,
             bitOffset=2,
             bitSize=1,
             base=pr.Bool))
 
-        self.add(pr.RemoteVariable(
-            name = 'ExtStartSrc',
+        self.add(pr.LinkVariable(
+            name = 'ExtAcquisitionSrc',
             mode = 'RW',
+            dependencies = [self.ExtAcquisitionSrcRaw, self.ExtAcquisitionEn],
+            disp = extTrigList,
+            linkedGet = extGet,
+            linkedSet = extSet))
+        
+
+        self.add(pr.RemoteVariable(
+            name = 'ExtStartSrcRaw',
+            mode = 'RW',
+            hidden = True,
             offset= 0x0C,
             bitOffset=0,
             bitSize=3,
@@ -73,10 +132,19 @@ class AcquisitionControl(pr.Device):
         self.add(pr.RemoteVariable(
             name = 'ExtStartEn',
             mode = 'RW',
+            hidden = True,
             offset= 0x10,
             bitOffset=3,
             bitSize=1,
             base=pr.Bool))
+
+        self.add(pr.LinkVariable(
+            name = 'ExtStartSrc',
+            mode = 'RW',
+            dependencies = [self.ExtStartSrcRaw, self.ExtStartEn],
+            disp = extTrigList,
+            linkedGet = extGet,
+            linkedSet = extSet))
         
 
         self.add(pr.RemoteVariable(
@@ -87,10 +155,22 @@ class AcquisitionControl(pr.Device):
             bitSize=1,
             base=pr.Bool))
 
-#         self.add(pr.RemoteCommand(
-#             name = "RegAcquisition",
-#             offset = 0x14,
-#             bitOffset = 0,
-#             bitSize = 1,
-#             function = pr.RemoteCommand.touchOne))
-        
+        for k,v in extTrigEnum.items():
+            self.add(pr.RemoteVariable(
+                name = f'{v}RisingEdgeCount',
+                mode = 'RO',
+                offset = 0x30 + k*4,
+                bitOffset = 0,
+                bitSize = 32,
+                base = pr.UInt,
+                disp = '{:d}'))
+
+        self.add(pr.RemoteCommand(
+            name = 'CountReset',
+            offset = 0x24,
+            bitOffset = 0,
+            bitSize = 1,
+            function = pr.RemoteCommand.touchOne))
+
+    def countReset(self):
+        self.CountReset()
