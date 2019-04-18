cc34db806eb64c81b110d877e3dc2320597f3f41
Benjamin Reese <bareese@slac.stanford.edu>
2019-04-18 16:34:58 -0700

    Add state registers back

--- software/python/KpixDaq/_KpixDataRx.py
+++ software/python/KpixDaq/_KpixDataRx.py
@@ -51,46 +51,44 @@ class KpixDataRx(pr.Device):
             bitOffset = 0,
             bitSize = 32))
 
-#         self.add(pr.RemoteVariable(
-#             name = 'RxState',
-#             mode = 'RO',
-#             offset = 0x24,
-#             bitOffset = 0,
-#             bitSize = 3,
-#             enum = {
-#                 0b000: "RX_IDLE_S",
-#                 0b001: "RX_HEADER_S",
-#                 0b010: "RX_ROW_ID_S",
-#                 0b011: "RX_DATA_S", 
-#                 0b100: "RX_FRAME_DONE_S",
-#                 0b101: "RX_DUMP_S",
-#                 0b110: "RX_RESP_S",
-#                 0b111: "INVALID"}));
-
-    #     self.add(pr.RemoteVariable(
-#             name = 'TxState',
-#             mode = 'RO',
-#             offset = 0x28,
-#             bitOffset = 0,
-#             bitSize = 4,
-#             enum = {
-#                 0b0000: "TX_CLEAR_S",
-#                 0b0001: "TX_IDLE_S",
-#                 0b0010: "TX_ROW_ID_S",
-#                 0b0011: "TX_NXT_COL_S", 
-#                 0b0100: "TX_CNT_S",
-#                 0b0101: "TX_TIMESTAMP_S",
-#                 0b0110: "TX_ADC_DATA_S",
-#                 0b0111: "TX_SEND_SAMPLE_S",
-#                 0b1000: "TX_WAIT_S",
-#                 0b1001: "TX_TEMP_S",
-#                 0b1010: "TX_RUNTIME_S",
-#                 0b1111: "INVALID" }));
+        
 
         self.add(pr.RemoteVariable(
-            name = 'Dummy',
+            name = 'RxState',
             mode = 'RO',
-            offset = 0x2C))
+            offset = 0x30,
+            bitOffset = 0,
+            bitSize = 3,
+            enum = {
+                0b000: "RX_IDLE_S",
+                0b001: "RX_HEADER_S",
+                0b010: "RX_ROW_ID_S",
+                0b011: "RX_DATA_S", 
+                0b100: "RX_FRAME_DONE_S",
+                0b101: "RX_DUMP_S",
+                0b110: "RX_RESP_S",
+                0b111: "INVALID"}));
+
+        self.add(pr.RemoteVariable(
+            name = 'TxState',
+            mode = 'RO',
+            offset = 0x34,
+            bitOffset = 0,
+            bitSize = 4,
+            enum = {
+                0b0000: "TX_CLEAR_S",
+                0b0001: "TX_IDLE_S",
+                0b0010: "TX_ROW_ID_S",
+                0b0011: "TX_NXT_COL_S", 
+                0b0100: "TX_CNT_S",
+                0b0101: "TX_TIMESTAMP_S",
+                0b0110: "TX_ADC_DATA_S",
+                0b0111: "TX_SEND_SAMPLE_S",
+                0b1000: "TX_WAIT_S",
+                0b1001: "TX_TEMP_S",
+                0b1010: "TX_RUNTIME_S",
+                0b1111: "INVALID" }));
+
         
 
         self.add(pr.RemoteCommand(
