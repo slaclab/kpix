4c1893c537dd19a79ecb2a03d86e440118e43f0a
Benjamin Reese <bareese@slac.stanford.edu>
2019-04-19 12:10:19 -0700

    Add rxShiftCount debug register

--- firmware/common/KpixDaq/rtl/KpixDataRx.vhd
+++ firmware/common/KpixDaq/rtl/KpixDataRx.vhd
@@ -307,6 +307,7 @@ begin
       axiSlaveRegister(axilEp, X"10", 0, v.resetCounters);
       axiSlaveRegisterR(axilEp, X"14", 0, r.frameCount);
       axiSlaveRegisterR(axilEp, X"20", 0, r.firstRuntime);
+      axiSlaveRegisterR(axilEp, X"24", 0, r.rxShiftCount);
       axiSlaveRegisterR(axilEp, X"30", 0, toSlv(RxStateType'pos(r.rxState), 4));
       axiSlaveRegisterR(axilEp, X"34", 0, toSlv(TxStateType'pos(r.txState), 4));
 
