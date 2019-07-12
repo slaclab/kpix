de098dcd3a88882ec447efaf18893df660a6be2e
Benjamin Reese <bareese@slac.stanford.edu>
2019-07-12 14:38:22 -0700

    Add debug flag

--- software/scripts/gui.py
+++ software/scripts/gui.py
@@ -8,7 +8,7 @@ pyrogue.addLibraryPath('../../firmware/submodules/surf/python')
 
 import KpixDaq
 
-rogue.Logging.setFilter('pyrogue.SrpV3', rogue.Logging.Debug)
+#rogue.Logging.setFilter('pyrogue.SrpV3', rogue.Logging.Debug)
 # Set the argument parser
 parser = argparse.ArgumentParser()
 
@@ -41,10 +41,17 @@ parser.add_argument(
     help     = "enable auto-polling",
 )
 
+parser.add_argument(
+    "--debug", 
+    required = False,
+    action = 'store_true',
+    help     = "enable auto-polling",
+)
+
 args = parser.parse_args()
 print(args)
 
-with KpixDaq.DesyTrackerRoot(debug=True, **vars(args)) as root:
+with KpixDaq.DesyTrackerRoot(**vars(args)) as root:
 
     # Create GUI
     appTop = pyrogue.gui.application(sys.argv)
