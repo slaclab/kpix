9b360055ff4ff0726202e80352b085345c9402e7
Benjamin Reese <bareese@slac.stanford.edu>
2019-08-01 15:48:42 -0700

    Hide Ltc4151, move Si7006 under EnvironmentMonitor

--- software/python/KpixDaq/_DesyTracker.py
+++ software/python/KpixDaq/_DesyTracker.py
@@ -147,13 +147,18 @@ class EnvironmentMonitor(pyrogue.Device):
         self.add(surf.devices.linear.Ltc4151(
             offset = 0x04000000,
             senseRes = 0.02,
-            hidden=False))
+            hidden=True))
 
         self.add(surf.devices.nxp.Sa56004x(
             description = "Board temperate monitor",
             offset = 0x04000400,
             hidden=True))
 
+        for i in range(4):
+            self.add(KpixDaq.Si7006(
+                name = f'Si7006[{i}]',
+                enabled = False,
+                offset = 0x07000000 + (i*0x1000)))
 
         self.add(pyrogue.LinkVariable(
             name = 'FpgaTemperature',
@@ -204,11 +209,6 @@ class DesyTracker(pyrogue.Device):
             expand = True))
 
         if not sim:
-            for i in range(4):
-                self.add(KpixDaq.Si7006(
-                    name = f'Si7006[{i}]',
-                    enabled = False,
-                    offset = 0x07000000 + (i*0x1000)))
             
             self.add(EnvironmentMonitor(expand=True))
 
